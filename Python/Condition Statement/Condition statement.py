7262baddf5c0610441dd9b0e6949f95260ebb5c0
Renatto <wstalinjacob@gmail.com>
2025-07-01 20:59:18 +0100

    dvd

---
+++ Python/Condition Statement/Condition statement.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a Number to check:"))
+print("Number to be checked :", number)
+
+if number %2==0 :
+    print("Number is an Even Number")
+
+else:
+    print("Number is an Odd Number")
