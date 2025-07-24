b160cca99e67b3bb0a1c53755ac5e23a1b8ea64a
Renatto <wstalinjacob@gmail.com>
2025-07-24 20:49:46 +0100

    dvd

---
+++ Python/Data 2.py/Tupple.py
@@ -0,0 +1,19 @@
+my_tuple = (1, "hello", 3.4)
+print(my_tuple)
+
+my_tuple1 = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple1)
+
+my_tuple = ('p','e','r','m','i','t')
+print(my_tuple[-5])
+print(my_tuple[5])
+
+
+
+print(my_tuple1[0][3])
+print(my_tuple1[1][1])
+
+print("Sliced :", my_tuple1[1:3])
+
+for letter in my_tuple1:
+    print(letter, end="")
