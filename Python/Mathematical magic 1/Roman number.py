c62271bdd228a72974ab12c49b7621784e52be6b
Renatto <princerenattostalinjacob@gmail.com>
2025-10-16 20:58:54 +0100

    dvd

--- Python/Mathematical magic 1/Roman number.py
+++ Python/Mathematical magic 1/Roman number.py
@@ -1,6 +1,6 @@
 def romanToInt(romanInput):
     roman = {'M': 1000, 'D': 500, 'C': 100,
-             'L': 50, 'X': 10, 'V': 5, '1': 1}
+             'L': 50, 'X': 10, 'V': 5, 'I': 1}
     resultInteger = 0
     for i in range(len(romanInput) - 1):
         if roman [romanInput[i] ]< roman[romanInput[i+1]]:
