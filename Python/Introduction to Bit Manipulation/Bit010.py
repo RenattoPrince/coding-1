40f7435a8b0bcafebacb56f307b1026f13177724
Renatto <princerenattostalinjacob@gmail.com>
2025-10-28 19:48:58 +0000

    dvd

---
+++ Python/Introduction to Bit Manipulation/Bit010.py
@@ -0,0 +1,23 @@
+num1 = 10 #1010
+num2 = 4 #0100
+
+#Using AND operator
+print("num1 & num2=", num1 & num2)
+
+#Using OR operator
+print("\num1 | num2 =", num1 | num2)
+
+#Using NOT operator
+print("\n-num1 =", ~num1)
+print("\num2 =", ~num2)
+
+#Using XOR operator
+print("\nnum1 ^ num2 =", num1 ^ num2)
+
+#Using Right Shift operator
+print("\nnum1 >> 1 =", num1 >> 1)
+print("num2 >> 1 =", num2 >> 1 )
+
+#Using Left Shift operator
+print("\nnum < 1 =", num1 << 1)
+print("num2 << 1 =", num2 << 1)
