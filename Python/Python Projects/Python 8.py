fd51fb2e74126cad93a51865c37796fa0a785157
Renatto <princerenattostalinjacob@gmail.com>
2025-11-15 12:17:41 +0000

    dvd

--- Python/Python Projects/Python 8.py
+++ Python/Python Projects/Python 8.py
@@ -1,1 +1,15 @@
-#
+#AllSubString
+def printAllSubString(all, SubString):
+
+    if len(all) == 0:
+        print(SubString)
+        return
+    firstChar = all[0]
+    restOfString = all[1:]
+    
+    printAllSubString(restOfString, SubString + firstChar)
+    
+    printAllSubString(restOfString, SubString)
+
+string = input("Enter a string: ")
+printAllSubString(string, "")
