0c18b63ddf89c59e20e5d7259101fd4afcb01e9f
Renatto <princerenattostalinjacob@gmail.com>
2025-11-18 19:33:49 +0000

    dvd

---
+++ Sequence Querry Language/1SQL start.sql
@@ -0,0 +1,23 @@
+CREATE TABLE students (
+    id INT,
+    name  VARCHAR(100),
+    age INT,
+    grade VARCHAR(10)
+);
+
+INSERT INTO students (id, name, age, grade)VALUES
+(01, 'Ali',17, 10),
+(21, 'Tanqif', 16, 9),
+(01, 'Ramin', 18, 11);
+select * from students;
+
+
+
+select * from students
+where age>=17;
+
+DROP TABLE students;
+
+
+select name,grade
+from students;
