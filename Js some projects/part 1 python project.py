702c4ada20b3b7a5130efafa76665343f0b1ca0f
RenattoPrince <wstalinjacob@gmail.com>
2026-02-17 20:29:39 +0000

    dvd

---
+++ Js some projects/part 1 python project.py
@@ -0,0 +1,47 @@
+import time
+
+print("Hello world!")
+
+# This is a comment, it will not be executed by the Python interpreter.
+# You can use comments to explain your code, or to temporarily disable a line of code.
+one = 1
+two = 2
+
+print("One plus two equals", one + two)
+
+Multiply = input("Enter the first number to multply: ")
+Multiply2 = input("Enter the second number to multiply: ")
+#The two inputs are stored by the computer and the computer will multiply them.
+Answer = int(Multiply) * int(Multiply2)
+print ("Your answer is: ", Answer)
+
+print("Another multiplication problem!")
+print("Now you will be multiplying this time.")
+350 == 350
+450 == 450
+
+print ("What is 350 multiplied by 450?")
+time.sleep(10)
+#This code will make the computer to wait for exactly 10 seconds before it will ask the user
+inpu67 = input("Enter your answer here: ")
+
+#This code will make the computer to wait for exactly 10 seconds before it will ask the user
+if int(inpu67) == 350 * 450:
+    print("Correct")
+else:    print("Incorrect, the correct answer is", 350 * 450)
+# This time the code will now use if and else statements to check if user answer is correct or not.
+# The second question will be the same math problems but with division.
+time.sleep(1)
+print("Now you will be doing some division problems.")
+time.sleep(0.75)
+print("Ready?")
+time.sleep(0.75)
+print("What is 10050 divided by 25?")
+time.sleep(10)
+hahah = 10050
+haha2 = 25
+Answer999 = int(hahah) / int(haha2)
+67 == input("Enter your answer here: ")
+if int(67) == Answer999:
+    print("Nice Job!")
+else:   print("Incorrect, the correct answer is", Answer999)
